6c0cad4494ca43589c766e06068102c6d027cfd7
mohammad <mohammadtohidi.mtn@gmail.com>
2022-12-25 13:17:35 +0330

    complet test

--- todo/tests.py
+++ todo/tests.py
@@ -1,11 +1,16 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
+from django.urls import reverse
+from .models import Todo
+
 
 # Create your tests here.
-class CreateApiTest(APITestCase):
+class TodoApiTest(APITestCase):
     def test_create(self):
         data = {"id": 1, "Title": "new task", "Description": "testing this",
                 "Date": "2022-10-23", "Completed": 'false'}
         response = self.client.post("/api/create", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertEqual(Todo.objects.get().Title, 'new task')
 
--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import Cred, ListTodo, CreateTodo
 
 urlpatterns = [
-    path('<int:pk>/', Cred.as_view(),),
+    path('<int:pk>/', Cred.as_view(),name='cred'),
     path('', ListTodo.as_view()),
     path('create', CreateTodo.as_view(),name='create'),
 ]
